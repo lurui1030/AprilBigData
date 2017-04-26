b4249e98b8e2acc2f7a7a4df12dcef09ce9025d0
Peiwen Yu <ypwen9124@hotmail.com>
2017-04-26 13:43:09 -0400

    update

--- homework2/run.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-hdfs dfs -rm -r /mututal
-hdfs dfs -rm -r /output
-hadoop com.sun.tools.javac.Main *.java
-jar cf friendRec.jar *.class
-hadoop jar friendRec.jar Drive input /mututal /output 5
-hdfs dfs -cat /output/*
