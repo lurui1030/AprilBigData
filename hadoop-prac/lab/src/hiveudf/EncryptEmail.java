74518c1cb07f49f26a91b5de1a6e58ae55b137ea
Zhang <zhangyunxiang0320@gmail.com>
2017-05-15 20:28:00 -0400

    hw4

--- hadoop-prac/lab/src/hiveudf/EncryptEmail.java
+++ hadoop-prac/lab/src/hiveudf/EncryptEmail.java
@@ -3,16 +3,12 @@ package hiveudf;
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.io.Text;
 
-import java.util.Arrays;
-
 public class EncryptEmail
     extends UDF {
   public Text evaluate(Text email) {
     try {
       int lastAt = email.toString().lastIndexOf("@");
-      char[] en = new char[lastAt];
-      Arrays.fill(en, 'X');
-      return new Text(new String(en) + email.toString().substring(lastAt));
+      return new Text("XXX" + email.toString().substring(lastAt));
     } catch (Exception e) {
       return new Text();
     }
